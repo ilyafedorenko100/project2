0194725ede4f9f7b771f366f7a5d6ae7a81d945b
ilya.fedorenko <ilya.fedorenko@x5.ru>
2020-09-03 14:07:40 +0300

    Задача 3 Домашнее задание 2

---
+++ src/package1/Cat.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class Cat {
+
+    String name;
+    int age;
+
+    public void setName(String newName) {
+        name = newName;
+
+    }
+
+}
--- src/package1/Main.java
+++ src/package1/Main.java
@@ -1,8 +1,8 @@
-package packege1;
+package package1;
 import java.util.Scanner;
-
 public class Main {
     public static void main (String[] args) {
+/*
         Scanner num = new Scanner(System.in);
         float first, second, result;
 
@@ -13,5 +13,19 @@ public class Main {
         result = first + second;
         result = result /2;
         System.out.println("Result is - " + result);
+*/
+
+        Scanner num = new Scanner(System.in);
+        int a;
+        System.out.print("Ввести целое число: ");
+        a = num.nextInt();
+
+        if (a>0)
+            System.out.println(a+1);
+        else
+            System.out.println(a);
+
     }
 }
+
+
